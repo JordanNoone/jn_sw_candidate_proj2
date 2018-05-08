e334b40a034b57ac1c29a86f20cc763abd71d851
Jordan Noone <jordan@relativityspace.com>
2018-05-07 18:20:55 -0700

    added whole influx folder

--- README.md
+++ README.md
@@ -3,11 +3,9 @@ response to homework sw_candidate_proj2
 
 # NOTES:
 - Influx Setup: https://docs.influxdata.com/influxdb/v1.5/introduction/getting-started/
-:
 
 # TODO:
 - Install docker with shell script
-- pull /etc/grafana/grafana.ini
 # Setup Instructions:
 - Start with vanilla Unbuntu 14.04
 - git clone https://github.com/JordanNoone/jn_sw_candidate_proj2.git
--- setupServices.bash
+++ setupServices.bash
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Grant Permission to git repo
+chmod -R a+rw ~/jn_sw_candidate_proj2/grafana/
+chmod -R a+rw ~/jn_sw_candidate_proj2/influxdb/
+
+
 # Clear Containers
 docker kill influxdb
 docker rm influxdb
@@ -8,7 +13,7 @@ docker rm grafana
 # Install Docker
 
 # Install InfluxDB
-docker run --name influxdb -d -p 8086:8086 -p 25826:25826/udp -v ~/influxdb:/var/lib/influxdb -v ~/jn_sw_candidate_proj2/influxdb/influxdb.conf:/etc/influxdb/influxdb.conf:rw influxdb
+docker run --name influxdb -d -p 8086:8086 -p 25826:25826/udp -v ~/influxdb:/var/lib/influxdb -v ~/jn_sw_candidate_proj2/influxdb/etc:/etc/influxdb/:rw influxdb
 # Configure InfluxDB
 
 # Install Grafana
